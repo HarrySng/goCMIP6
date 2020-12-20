593c2a2562c33ceeb933350f8d2fa7d2c1e3f2f9
harrysng <hsing247@uwo.ca>
2020-12-20 02:47:24 -0800

    Downscaling working copy

--- downscale.sh
+++ downscale.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
+# Directory structure
+# ./
+#	downscale.sh
+#	rawnc/
+#		getnc.py
+#		sproket
+#		target_grid
+#	ncfiles/
 
-# Download wget scripts from https://esgf-node.llnl.gov/search/cmip6/
-#Create the following directory structure
-##########################################
-# parent/                                #
-#        downscale.sh                    #
-#        target_grid                     #
-#        shellFiles/                     #
-#                  all wget scripts      #
-##########################################
-# chmod +x to all wget scripts to make them executable
+# This script runs in parent directory
 
-for file in ./shellFiles/* # For each wget script
+cd rawnc
+
+for var in pr tasmax tasmin # 1st loop over variables
+do # 1st loop do
+for exp in historical ssp245 # 2nd loop over experiment
+do # 2nd loop do
+for mdl in CanESM5 # 3rd loop over model
+do # 3rd loop do
+python getnc.py $var $exp $mdl # This will create params.json
+./sproket -config params.json # Downloads the netcdf file according to parameters in json file
+while [ ! -s *.nc ] # While the file is download, its extension is nc_0, so wait till .nc exists
 do
-  sh $file -s # execute the script which downloads the nc file in parent/ directory
+	: # Do nothing till file completely downloaded
 done
-
-for file in *.nc # For each netcdf file (global raw file)
-do
-  var=$(echo $file | cut -d'_' -f 1-4) # Extract a substring of its name to make it easier to interpret
-  mv $file ${var}.nc # Rename the file
+mv *.nc ${var}_${exp}_${mdl}.nc # Rename the file
+ncks -d lat,43.,54. -d lon,65.,95. ${var}_${exp}_${mdl}.nc -O ${var}_${exp}_${mdl}_subset.nc # Subset over study area
+cdo remapbil,target_grid ${var}_${exp}_${mdl}_subset.nc ${var}_${exp}_${mdl}_d.nc # 
+rm -f ${var}_${exp}_${mdl}_subset.nc ${var}_${exp}_${mdl}.nc # Cleanup
+mv ${var}_${exp}_${mdl}_d.nc ../ncfiles/${var}_${exp}_${mdl}_d.nc
 done
-
-for file in *.nc # For each renamed file (still global raw file)
-do
-  var=$(echo $file | cut -d'.' -f 1) # Extract the substring name again, will use later
-  ncks -d lat,43.,54. -d lon,65.,95. $file -O ${var}_subset.nc # Clip the file to a bounding box bigger than observation area
-  rm -f $file # Clean up
-  cdo remapbil,target_grid ${var}_subset.nc $file # Downscale the file using bilinear interpolation
-  rm -f ${var}_subset.nc # Clean up
+done
 done
---
+++ rawnc/getnc.py
@@ -0,0 +1,23 @@
+import sys
+import json
+
+var = sys.argv[1]
+experiment = sys.argv[2]
+source = sys.argv[3]
+
+
+dict = {
+	"search_api": "https://esgf-node.llnl.gov/esg-search/search/",
+	"data_node_priority": ["aims3.llnl.gov", "esgf-data1.llnl.gov"],
+	"fields": {
+		"variable_id": var,
+		"experiment_id": experiment,
+		"source_id": source,
+		"table_id": "Amon",
+        "variant_label": "r1i1p1f1",
+        "project": "CMIP6"
+	}
+}
+
+with open('params.json', 'w') as outfile:
+    json.dump(dict, outfile)
